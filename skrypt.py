805fcb7744f974834afb0495dcb8d437b9a0f40c
Verten294 <146519878+Verten294@users.noreply.github.com>
2023-09-30 13:02:50 +0200

    Delete skrypt.py

--- skrypt.py
+++
@@ -1,9 +0,0 @@
-name = input (" Podaj imię ")
-weather = input (" Jaka jest pogoda? ")
-print(" Witaj " + name  )
-
-print( weather )
-if weather == "pada":
-        print("lepiej zostań w domu")
-else:
-        print("wyjdź na zewnątrz!")
